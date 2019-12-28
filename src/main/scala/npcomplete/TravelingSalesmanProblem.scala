f92aa739b27ae278c564c6f805d207fa837b6a75
mdfecioru <mdfecioru@gmail.com>
2019-12-28 12:20:12 -0800

    Updating traveling salesman for test.

--- src/main/scala/npcomplete/TravelingSalesmanProblem.scala
+++ src/main/scala/npcomplete/TravelingSalesmanProblem.scala
@@ -4,71 +4,74 @@ import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
-object TravelingSalesmanProblem extends App {
+object TravelingSalesmanProblem {
+  final val INFINITY = Int.MaxValue
 
-  val INFINITY = Int.MaxValue
-  val adjMatrix = new ArrayBuffer[ArrayBuffer[Int]]()
+  def readGraphFromFile(filename: String): ArrayBuffer[ArrayBuffer[Int]] = {
+    val iter = Source.fromFile(filename).getLines()
+    val nrVertex = iter.next().toInt
+    val adjMatrix = new ArrayBuffer[ArrayBuffer[Int]](nrVertex)
 
-  val filename = "src/main/resources/tsp_small.txt"
-  val iter = Source.fromFile(filename).getLines()
-  val nrVertex =iter.next().toInt
-  val solution = iter.next().toInt
-
-  for (_ <- 0 to nrVertex-1) {
-    val arr0 = new ArrayBuffer[Int](nrVertex)
-    for (_ <- 0 to nrVertex-1) {
-      arr0.addOne(INFINITY)
+    for (_ <- 0 to nrVertex - 1) {
+      val arr0 = new ArrayBuffer[Int](nrVertex)
+      for (_ <- 0 to nrVertex - 1) {
+        arr0.addOne(INFINITY)
+      }
+      adjMatrix.addOne(arr0)
     }
-    adjMatrix.addOne(arr0)
+
+    for (line <- iter) {
+      val items = line.split(" ")
+      val v1 = items(0).toInt - 1
+      val v2 = items(1).toInt - 1
+      val w = items(2).toInt
+      adjMatrix(v1)(v2) = w
+      adjMatrix(v2)(v1) = w
+    }
+
+    adjMatrix
   }
 
-  for (line <- iter) {
-    val items = line.split(" ")
-    val v1 = items(0).toInt - 1
-    val v2 = items(1).toInt - 1
-    val w = items(2).toInt
-    adjMatrix(v1)(v2) = w
-    adjMatrix(v2)(v1) = w
-  }
-
-  var curr = new ArrayBuffer[ArrayBuffer[Int]]()
-  var hmCurr = new mutable.HashMap[String, mutable.HashMap[Int, Int]]()
-  for (i <- 1 to nrVertex-1) {
-    val arr0 = new ArrayBuffer[Int]()
-    arr0.addOne(i)
-    curr.addOne(arr0)
-    val hmv = new mutable.HashMap[Int, Int]()
-    hmCurr.addOne(i.toString(), hmv)
-    hmv.addOne(i, adjMatrix(0)(i))
-  }
-
-  for (i <- 2 to nrVertex-1) {
-    curr = generateCombination(curr, nrVertex-1)
-    val hmPrev = hmCurr
-    hmCurr = new mutable.HashMap[String, mutable.HashMap[Int, Int]]()
-    for (combination <- curr) {
+  def run(adjMatrix: ArrayBuffer[ArrayBuffer[Int]]): Int = {
+    val nrVertex = adjMatrix.size
+    var curr = new ArrayBuffer[ArrayBuffer[Int]]()
+    var hmCurr = new mutable.HashMap[String, mutable.HashMap[Int, Int]]()
+    for (i <- 1 to nrVertex - 1) {
+      val arr0 = new ArrayBuffer[Int]()
+      arr0.addOne(i)
+      curr.addOne(arr0)
       val hmv = new mutable.HashMap[Int, Int]()
-      hmCurr.addOne(generateHashKey(combination), hmv)
-      for (j <- combination) {
-        val c = new ArrayBuffer[Int]()
-        c.addAll(combination)
-        c -= j
-        var minVal = INFINITY
-        for (k <- c) {
-          minVal = Math.min(minVal, hmPrev(generateHashKey(c))(k) + adjMatrix(k)(j))
+      hmCurr.addOne(i.toString(), hmv)
+      hmv.addOne(i, adjMatrix(0)(i))
+    }
+
+    for (i <- 2 to nrVertex - 1) {
+      curr = generateCombination(curr, nrVertex - 1)
+      val hmPrev = hmCurr
+      hmCurr = new mutable.HashMap[String, mutable.HashMap[Int, Int]]()
+      for (combination <- curr) {
+        val hmv = new mutable.HashMap[Int, Int]()
+        hmCurr.addOne(generateHashKey(combination), hmv)
+        for (j <- combination) {
+          val c = new ArrayBuffer[Int]()
+          c.addAll(combination)
+          c -= j
+          var minVal = INFINITY
+          for (k <- c) {
+            minVal = Math.min(minVal, hmPrev(generateHashKey(c))(k) + adjMatrix(k)(j))
+          }
+          hmv.addOne(j, minVal)
         }
-        hmv.addOne(j, minVal)
       }
     }
-  }
 
-  var minV = INFINITY
-  for ((_, hmv) <- hmCurr) {
-    hmv foreach( x => minV = Math.min(minV, x._2 + adjMatrix(0)(x._1)))
+    var minV = INFINITY
+    for ((_, hmv) <- hmCurr) {
+      hmv foreach (x => minV = Math.min(minV, x._2 + adjMatrix(0)(x._1)))
+    }
+
+    minV
   }
-  println("minV = ", minV)
-  if (minV == solution) println("Test passed.")
-  else println("Test FAILED!")
 
   def printArr(arr: ArrayBuffer[ArrayBuffer[Int]]) = {
     for (line <- arr) {
@@ -82,6 +85,7 @@ object TravelingSalesmanProblem extends App {
     }
     println()
   }
+
   def generateCombination(currentStep: ArrayBuffer[ArrayBuffer[Int]], size: Int): ArrayBuffer[ArrayBuffer[Int]] = {
     val next = new ArrayBuffer[ArrayBuffer[Int]]()
     for (c <- currentStep) {
@@ -95,6 +99,7 @@ object TravelingSalesmanProblem extends App {
     }
     next
   }
+
   def generateHashKey(arr: ArrayBuffer[Int]): String = {
     var s = ""
     for (elem <- arr) {
@@ -102,4 +107,5 @@ object TravelingSalesmanProblem extends App {
     }
     s.dropRight(1)
   }
+
 }
